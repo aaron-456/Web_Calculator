fa94af911fff74c695fe626037cd81501105b412
aaron-456 <aaronburbanoms15@gmail.com>
2023-08-11 03:38:22 -0500

    Correction of errors and addition of reset functionality

--- script.js
+++ script.js
@@ -10,24 +10,62 @@ function displayNumbers(value) {
 }
 
 function displayOperators(operator) {
-  const operators = document.getElementById("containerLogic");
   currentValue = operator;
   valorPasado = valorActual;
+  const operators = document.getElementById("containerLogic");
+  operators.textContent = "";
+}
 
-  operators.textContent = operator;
+function resetear() {
+  const reset = document.getElementById("containerLogic");
+  reset.textContent = "";
 }
 
 function calcular() {
   const calculation = document.getElementById("containerLogic");
-  const valorP = valorActual;
+  const valorPrincipal = valorActual;
 
   if (currentValue === "+") {
-    calculation.textContent = parseInt(valorP) + parseInt(valorPasado);
+    calculation.textContent = valorPasado + valorPrincipal;
   } else if (currentValue === "-") {
-    calculation.textContent = parseInt(valorP) + parseInt(valorPasado);
+    calculation.textContent = valorPasado - valorPrincipal;
   } else if (currentValue === "*") {
-    calculation.textContent = parseInt(valorP) + parseInt(valorPasado);
+    calculation.textContent = valorPasado * valorPrincipal;
   } else if (currentValue === "/") {
-    calculation.textContent = valorP / valorPasado;
+    calculation.textContent = valorPasado / valorPrincipal;
   }
+}
+
+{
+  // let currentValue = ""; //operador actual
+  // let valorActual = 0; //Valor actual
+  // let valorPasado = 0;
+  // //Esta funcion se encarga de aser que se reflejen los numeros en el div donde se vana a almacenar
+  // function displayNumbers(value) {
+  //   const numbers = document.getElementById("containerLogic");
+  //   numbers.textContent += value;
+  //   valorActual = parseInt(numbers.textContent);
+  // }
+  // function displayOperators(operator) {
+  //   const operators = document.getElementById("containerLogic");
+  //   currentValue = operator;
+  //   valorPasado = valorActual;
+  //   operators.textContent = operator;
+  // }
+  // function calcular() {
+  //   const calculation = document.getElementById("containerLogic");
+  //   const valorP = valorActual;
+  //   if (currentValue === "+") {
+  //     calculation.textContent = parseInt(valorP) + parseInt(valorPasado);
+  //   }
+  //   if (currentValue === "-") {
+  //     calculation.textContent = parseInt(valorPasado) - parseInt(valorP);
+  //   }
+  //   if (currentValue === "*") {
+  //     calculation.textContent = parseInt(valorP) * parseInt(valorPasado);
+  //   }
+  //   if (currentValue === "/") {
+  //     calculation.textContent = parseFloat(valorPasado) / parseFloat(valorP);
+  //   }
+  // }
 }
